6ef4b13e8fd758506447273f0292689cf80e6c3c
Achille Beltrami <achille.beltrami@gmail.com>
2026-01-09 18:36:23 +0100

    fix: import useTabaccai via direct file path

--- components/crm/tabaccai/TabaccaiClient.tsx
+++ components/crm/tabaccai/TabaccaiClient.tsx
@@ -2,7 +2,7 @@
 
 import { useMemo, useState } from "react";
 import TabaccaiList from "./TabaccaiList";
-import { useTabaccai } from "./data";
+import { useTabaccai } from "./data/useTabaccai";
 
 export default function TabaccaiClient() {
   // 🔗 DATI CENTRALIZZATI
